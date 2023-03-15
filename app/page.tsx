7f64f98ef7a001cd3e9c230732c6639ca0311308
Marco <marco.wyatt@rocketmail.com>
2023-03-15 14:51:32 +0000

    added static image

--- app/page.tsx
+++ app/page.tsx
@@ -43,7 +43,7 @@ export default function Home() {
         <div className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mt-0 lg:mr-0 lg:max-w-none lg:flex-none xl:ml-32">
           <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
             <Image
-              src="/../public/Preview.png"
+              src="/static/Preview.png"
               alt="App screenshot"
               width={2938}
               height={1840}
