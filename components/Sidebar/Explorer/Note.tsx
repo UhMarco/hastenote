792628052ad386fac4976874fd45489cbd40db10
UhMarco <ddefygravity@gmail.com>
2023-03-15 22:22:10 +0000

    dragging improvements

--- components/Sidebar/Explorer/Note.tsx
+++ components/Sidebar/Explorer/Note.tsx
@@ -76,13 +76,19 @@ export default function Note({ note }: { note: Note; }) {
 
   // Moving notes
   const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
-    console.log(e);
-
+    console.log("Drag End");
+    console.log(document.elementFromPoint(e.pageX, e.pageY));
   };
 
   return (
     <>
-      <div draggable={!renaming} onDragEnd={handleDrop} onContextMenu={handleContextMenu} onClick={() => { if (!renaming) activeNote.setNote(note); }} className={`-ml-2 pl-2 flex flex-shrink-0 items-center cursor-pointer focus:cursor-grabbing pr-4 hover:text-text-xlight${activeNote.note?.note_id === note.note_id ? " bg-white/5 bg-cover" : ""}${menu ? " text-text-xlight" : ""}`}>
+      <div
+        draggable={!renaming}
+        onDragEnd={handleDrop}
+        onContextMenu={handleContextMenu}
+        onClick={() => { if (!renaming) activeNote.setNote(note); }}
+        className={`-ml-2 pl-2 cursor-pointer flex flex-shrink-0 items-center pr-4 hover:text-text-xlight${activeNote.note?.note_id === note.note_id ? " bg-white/5 bg-cover" : ""}${menu ? " text-text-xlight" : ""}`}
+      >
         <svg className="w-4 h-4 flex-shrink-0 ml-[18px] mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}>
           <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
         </svg>
